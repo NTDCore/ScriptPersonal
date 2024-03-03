12f014b6713cbf8f8a701e81fe417b00fbc5f1e5
Monia <102583640+NTDCore@users.noreply.github.com>
2024-03-03 19:28:27 +0700

    Create Hider.lua

---
+++ Hider.lua
@@ -0,0 +1,27 @@
+-- i will say this shit is just name hider
+local players = game.Players
+local lplr = players.LocalPlayer
+local hiderdata = {
+	['Yourself'] = {
+		['Name'] = '???',
+		['DisplayName'] = '???'
+	},
+	['Everyone'] = {
+		['Name'] = 'Name',
+		['DisplayName'] = 'Name'
+	}
+}
+
+local playerstable = {}
+for i,v in pairs(players:GetPlayers()) do
+	table.insert(playerstable, v)
+	table.remove(playerstable, 1)
+end
+
+lplr.Name = hiderdata['Yourself']['Name']
+lplr.DisplayName = hiderdata['Yourself']['DisplayName']
+
+for i,v in pairs(playerstable) do
+	v.Name = hiderdata['Everyone']['Name']
+	v.DisplayName = hiderdata['Everyone']['DisplayName']
+end
