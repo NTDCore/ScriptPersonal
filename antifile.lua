57377a8ab4798b947b3100d543bbba4721fe5181
Monia <102583640+NTDCore@users.noreply.github.com>
2024-03-06 18:54:31 +0700

    Update antifile.lua

--- antifile.lua
+++ antifile.lua
@@ -1,1 +1,36 @@
 -- Working in Progress
+local loadfilefunc = loadfile or function(file)
+	return loadstring(readfile(file))()
+end
+local hookfuncs = hookfunc or hookfunction -- hookfunc is allias from krampus lol
+local oldreadfile
+local olddelfile
+local olddelfolder
+local oldisfile
+oldreadfile = hookfuncs(readfile, newcclosure(function(file)
+	if file:find('6872274481') then
+		return 'here my source haha'
+	end
+	return oldreadfile(file)
+end))
+
+olddelfile = hookfuncs(delfile, newcclosure(function(file)
+	if file:find('6872274481.lua') then
+		return nil
+	end
+	return olddelfile(file)
+end))
+
+olddelfolder = hookfuncs(delfolder, newcclosure(function(file)
+	if file:find('vape') or file:find('vapeprivate') then
+		return nil
+	end
+	return olddelfolder(file)
+end))
+
+oldisfile = hookfuncs(isfile, newcclosure(function(file)
+	if file:find('vapeprivate') or file:find('vape') then
+		return nil
+	end
+	return oldisfile(file)
+end))
