634c0707e1c011e256673a0f52372bfe12c7cf9d
Monia <102583640+NTDCore@users.noreply.github.com>
2025-02-15 21:38:53 +0700

    Update HttpSpy.lua

--- HttpSpy.lua
+++ HttpSpy.lua
@@ -15,44 +15,53 @@ local HttpSpySettings = {
 	['AntiKick'] = shared.HttpSpy.AntiKick or true,
 	['AntiFPS'] = shared.HttpSpy.AntiFPS or false
 }
-local hookmeta = hookmetamethod
-local websock = WebSocket and WebSocket.Connect or WebSocket and WebSocket.connect
-local hookfunct = hookfunc or hookfunction -- hookfunc in krampus lol
-local restoreFuncs = restorefunction or restorefunc
+local httpSpyDetected = {
+	urls = {
+		'github',
+		'pastebin',
+		'ip-api'
+	},
+	malicious = {
+		'grabify',
+		'discord',
+		'webhook',
+		'ip-api'
+	}
+}
+
+local cloneref = cloneref or function(ref) return ref end
+local websockets = WebSocket and WebSocket.Connect or WebSocket and WebSocket.connect
+local hookfunctions = hookfunc or hookfunction -- hookfunc in krampus lol
 local requests = Fluxus and Fluxus.request or fluxus and fluxus.request or http_request or http.request or request
-local lplr = game.Players.LocalPlayer
-local startergui = game:GetService('StarterGui')
+local lplr = cloneref(game:GetService('Players')).LocalPlayer -- my friend be doin' e-sex in discord
+local startergui = cloneref(game:GetService('StarterGui'))
 local sendNotification = function(name, info, delay)
 	startergui:SetCore('SendNotification', {
-		Title = name,
-		Text = info,
+		Title = name or 'Roblox',
+		Text = info or 'sigma sigma boy',
 		Duration = delay or 5
 	})
 end
-if not isfolder("httpSpy") then
-	makefolder("httpSpy")
+if not isfolder('httpSpy') then
+	makefolder('httpSpy')
 end
 local writeDirectFile = function(file, code)
-	return writefile("httpSpy/"..file, code)
+	return writefile('httpSpy/'..file, code)
 end
 local detectLink = function(file, logname, code)
-	if (not isfile("httpSpy/"..file)) then
-		writeDirectFile(file, "-- "..logname.." Log\n--HttpSpy Made By Monia\n--Logging Start:\n"..code)
-	elseif isfile("httpSpy/"..file) then
-		appendfile("httpSpy/"..file, "\n"..code)
+	if (not isfile('httpSpy/'..file)) then
+		writeDirectFile(file, '-- '..logname..' Log\n--HttpSpy Made By Monia\n--Logging Start:\n'..code)
+	elseif isfile('httpSpy/'..file) then
+		appendfile('httpSpy/'..file, '\n'..code)
 	end
-end
-
-if getgenv().hookfunction == nil or getgenv().hookfunc == nil then
-	restoreFuncs(hookfunct)
 end
 
 -- // Anti Log \\ --
 local oldrequest
-oldrequest = hookfunct(requests, newcclosure(function(req)
-	if req.Url:find("discord") and req.Url:find('webhooks') or not req.Url:find('discord') and not req.Url:find('webhooks') then
+oldrequest = hookfunctions(requests, newcclosure(function(req)
+	if req.Url:find('discord') and req.Url:find('webhooks') or not req.Url:find('discord') and not req.Url:find('webhooks') then
 		print('Detected Request: '.. req.Url)
-		detectLink("request.log", "Request", req.Url)
+		detectLink('request.log', 'Request', req.Url)
 		if HttpSpySettings['Notifications'] then
 			sendNotification('Request', 'request detected: '.. req.Url)
 		end
@@ -65,10 +74,10 @@ end))
 
 -- // Spy #1 \\ --
 local oldHttpGet
-oldHttpGet = hookfunct(game.HttpGet, newcclosure(function(newgame, url)
-	if url:find("github") or url:find("pastebin") or not url:find('github') or not url:find('pastebin') then
+oldHttpGet = hookfunctions(game.HttpGet, newcclosure(function(newgame, url)
+	if url:find('github') or url:find('pastebin') or not url:find('github') or not url:find('pastebin') then
 		print('Detected Link: '.. url)
-		detectLink("Http.log", "Http", url)
+		detectLink('Http.log', 'Http', url)
 		if HttpSpySettings['Notifications'] then
 			sendNotification('HttpGet', 'http detected: '.. url)
 		end
@@ -79,14 +88,31 @@ oldHttpGet = hookfunct(game.HttpGet, newcclosure(function(newgame, url)
 	return oldHttpGet(newgame, url)
 end))
 
+old = hookmetamethod(game, '__namecall', newcclosure(function(self, url, ...)
+	if getnamecallmethod() == 'HttpGet' then
+		local retur = true
+		for i,v in 	httpSpyDetected.malicious do
+			if url:find(v) then
+				detectLink('Http.log', 'Http', url)
+				retur = true
+				break
+			end
+		end
+		if retur then
+			return old(self, '', ...)
+		end
+	end
+	return old(self, url, ...)
+end))
+
 --[[
 --// In Dev \\ --
 -- // Spy #2 \\ --
 local oldHttpGetAsync
-oldHttpGetAsync = hookfunct(game.HttpGetAsync, newcclosure(function(newgame, url)
-	if url:find("github") or url:find("pastebin") or not url:find('github') or not url:find('pastebin') then
+oldHttpGetAsync = hookfunctions(game.HttpGetAsync, newcclosure(function(newgame, url)
+	if url:find('github') or url:find('pastebin') or not url:find('github') or not url:find('pastebin') then
 		print('Detected Link: '.. url)
-		detectLink("Http.log", "Http", url)
+		detectLink('Http.log', 'Http', url)
 		if HttpSpySettings['Notifications'] then
 			sendNotification('HttpGet', 'http detected: '.. url)
 		end
@@ -106,7 +132,7 @@ if HttpSpySettings['AntiKick'] then
 	mt.__namecall = newcclosure(function(self, ...)
 		local args = {...}
 		local namecallmethod = getnamecallmethod()
-		if self == plr and string.lower(namecallmethod) == "kick" then
+		if self == plr and string.lower(namecallmethod) == 'kick' then
 			task.wait(math.huge)
 			return nil
 		elseif string.lower(namecallmethod) == 'shutdown' then
@@ -135,7 +161,7 @@ if HttpSpySettings['AntiFPS'] then
 	local minusFps = function()
 		return -1 or -2 or -3 or -4 or -5 or -6 or -7 or -8 or -9
 	end
-	oldcrash = hookfunct(fpsfunc, newcclosure(function(value)
+	oldcrash = hookfunctions(fpsfunc, newcclosure(function(value)
 		-- wtf is this
 		if value:len() >= 10 and value:sub(1,10) == nineEnine() or value:len() >= 3 and value:sub(1,3) == 9e9 or value:len() >= 9 and value:sub(1,9) == math.huge or value:len() >= 1 and value:sub(1,1) == 0 or value:len() >= 1 and value:sub(1,1) == oneFps() or value:len() >= 2 and value:sub(1,2) == minusFps() or value:len() >= 2 and value:sub(1,3) == tenFps() then
 			return 144
