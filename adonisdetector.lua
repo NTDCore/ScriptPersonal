109381005d84bb545497e86697a9fa4c934e2c17
Moon <114739237+MOONCoreDev@users.noreply.github.com>
2025-03-30 09:29:21 +0700

    update adonisdetector.lua

---
+++ adonisdetector.lua
@@ -0,0 +1,25 @@
+repeat task.wait() until game:IsLoaded()
+local cloneref = cloneref or function(...) return ... end
+
+local players = cloneref(game:GetService('Players'))
+local lplr = players.LocalPlayer
+local replicatedStorage = cloneref(game:GetService('ReplicatedStorage'))
+
+local getgc = getgc or function() end
+local rawget = rawget or function() end
+
+for i,v in getgc(true) do
+	if type(v) == 'table' then
+		local adonis = {
+			Detected = rawget(v, 'Detected'),
+			Kill = rawget(v, 'Kill'),
+			GPUCrash = rawget(v, 'GPUCrash')
+		}
+		for a, b in adonis do
+			if b ~= nil and type(b) == 'function' then
+				print('Adonis detected: '..getinfo(b).name)
+				return
+			end
+		end
+	end
+end
